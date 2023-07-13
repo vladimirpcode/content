1c835dda4fc4ea22b9d36398a06b897aaa10879c
Vladimir Ponomarenko <v2ponomarenko@gmail.com>
2023-07-13 23:51:35 +0300

    01/работает

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
 
 using namespace std;
 
@@ -15,7 +17,8 @@ enum class Lex{
     DIVIDE,
     LBRACE,
     RBRACE,
-    EOT
+    EOT,
+    NONE
 };
 
 class Node{
@@ -23,6 +26,24 @@ public:
     Node* left;
     Node* right;
     Lex operation;
+    double value;
+
+    Node(){
+        left = nullptr;
+        right = nullptr;
+        operation = Lex::NONE;
+        value = 0;
+    }
+
+    ~Node(){
+        if (left != nullptr){
+            delete left;
+        }
+        if (right != nullptr){
+            delete right;
+        }
+    }
+
 };
 
 void error(string msg){
@@ -44,7 +65,7 @@ bool is_number(char c){
 }
 
 bool is_alpha(char c){
-    return (c >= 65 && c <= 90) && (c >= 97 && c <= 122);
+    return (c >= 65 && c <= 90) || (c >= 97 && c <= 122);
 }
 
 class Wrapper{
@@ -107,7 +128,7 @@ public:
                 case ')': token = Lex::RBRACE; break;
                 case '=': token = Lex::EQUAL; break;
                 case EOT: token = Lex::EOT; return token;
-                default: error("неизвестный токен " + wrap.ch);
+                default: cout << wrap.ch; error("неизвестный токен");
             }
             wrap.get_next();
             return token;
@@ -135,22 +156,107 @@ private:
     }
 };
 
+
+Lexer* lexer;
+map<string, double> context;
+
 ////////////////////////
+void term(Node *&node);
+void factor(Node *&node);
+
 // парсер
 // Выражение = Слагаемое {ЗнакСлож Слагаемое}
 // Слагаемое = Множитель {ЗнакУмнож Множитель}
 // Множитель = Число | Идентификатор | "(" Выражение ")"
 // ЗнакСлож = "+" | "-"
 // ЗнакУмнож = "*" | "/"
-void expression(){
-    
+void expression(Node *&node){
+    term(node);
+    while (lexer->token == Lex::MINUS || lexer->token == Lex::PLUS){
+        Node *tmp = node;
+        node = new Node();
+        node->operation = lexer->token;
+        node->left = tmp;
+        lexer->next_lex();
+        node->right = new Node();
+        term(node->right);
+    }
+}
+
+void term(Node *&node){
+    factor(node);
+    while (lexer->token == Lex::MULTIPLY || lexer->token == Lex::DIVIDE){
+        Node *tmp = node;
+        node = new Node();
+        node->left = tmp;
+        node->operation = lexer->token;
+        lexer->next_lex();
+        node->right = new Node();
+        factor(node->right);
+    }
+}
+
+void factor(Node *&node){
+    if (lexer->token == Lex::LBRACE){
+        lexer->next_lex();
+        expression(node);
+        if (lexer->token != Lex::RBRACE){
+            expected(")");
+        }
+        lexer->next_lex();
+    } else if (lexer->token == Lex::NUMBER){
+        node->value = lexer->number_value;
+        lexer->next_lex();
+    } else if (lexer->token == Lex::IDENT){
+        if (context.find(lexer->name) == context.end()){
+            error("имя  не существует");
+        }
+        node->value = context[lexer->name];
+        lexer->next_lex();
+    }
+}
+
+double calculate(Node* node){
+    if (node->left == nullptr && node->right == nullptr){
+        return node->value;
+    }
+    double left_result = calculate(node->left);
+    double right_result = calculate(node->right);
+    switch (node->operation){
+        case Lex::PLUS: return left_result + right_result;
+        case Lex::MINUS: return left_result - right_result;
+        case Lex::DIVIDE: return left_result / right_result;
+        case Lex::MULTIPLY: return left_result * right_result;
+        default: cout << "U" << (int)node->operation << "__" << node->value<<"|"; return left_result;
+    }
+    return 0;
 }
 
 ////////////////////////
-
+// соглашение: каждая процедура получает инициализированный указатель на ноду
 int main()
 {
-    
+    context["myVar"] = 100;
+    context["pi"] = 3.14;
+    vector<string> exprs = {
+        "2+2", 
+        "10 * 2 + 7", 
+        "((2 + 100 * 7) + 1) / 2",
+        "((2 + myVar * 7) + 1) / 2",
+        "pi * 2 * 2",
+        "(2+2)*(3+3)",
+        "2 + 2 + 2",
+        "(1 + 3 + (4 * 5 - (7/2 + (2+5-4))))"
 
+    };
+    for (auto& expr:exprs){
+        cout << expr << " = ";
+        lexer = new Lexer(expr);
+        Node* tree = new Node();
+        expression(tree);
+        cout << calculate(tree) << endl;
+        delete tree;
+        delete lexer;
+    }
     return 0;
 }
